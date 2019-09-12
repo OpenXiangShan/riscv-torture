0243ad03f5ba192652fbe17e4a45936dc74ce630
Zihao Yu <yuzihao@ict.ac.cn>
2019-09-12 10:03:47 +0000

    generator,main: add -d option to specify the output directory

--- generator/src/main/scala/main.scala
+++ generator/src/main/scala/main.scala
@@ -7,7 +7,7 @@ import java.io.FileInputStream
 import java.util.Properties
 import scala.collection.JavaConversions._
 
-case class Options(var outFileName: String = "test",
+case class Options(var outFileName: String = "test", var outDirName: String = "output",
   var confFileName: String = "config/default.config")
 
 object Generator extends App
@@ -16,17 +16,18 @@ object Generator extends App
   {
     val parser = new OptionParser[Options]("generator/run") {
       opt[String]('C', "config") valueName("<file>") text("config file") action {(s: String, c) => c.copy(confFileName = s)}
+      opt[String]('d', "directory") valueName("<dir>") text("ouput directory") action {(s: String, c) => c.copy(outDirName = s)}
       opt[String]('o', "output") valueName("<filename>") text("output filename") action {(s: String, c) => c.copy(outFileName = s)}
     }
     parser.parse(args, Options()) match {
       case Some(opts) =>
-        generate(opts.confFileName, opts.outFileName)
+        generate(opts.confFileName, opts.outDirName, opts.outFileName)
       case None =>
         System.exit(1) //error message printed by parser
     }
   }
 
-  def generate(confFile: String, outFileName: String): String = {
+  def generate(confFile: String, outDirName: String, outFileName: String): String = {
     val config = new Properties()
     val in = new FileInputStream(confFile)
     config.load(in)
@@ -42,10 +43,10 @@ object Generator extends App
     val segment = (config.getProperty("torture.generator.segment", "true").toLowerCase == "true")
     val loop    = (config.getProperty("torture.generator.loop", "true").toLowerCase == "true")
     val loop_size = config.getProperty("torture.generator.loop_size", "256").toInt
-    generate(nseqs, memsize, fprnd, mix, vec, use_amo, use_mul, use_div, outFileName, segment, loop, loop_size)
+    generate(nseqs, memsize, fprnd, mix, vec, use_amo, use_mul, use_div, outDirName, outFileName, segment, loop, loop_size)
   }
 
-  def generate(nseqs: Int, memsize: Int, fprnd : Int, mix: Map[String,Int], veccfg: Map[String,String], use_amo: Boolean, use_mul: Boolean, use_div: Boolean, outFileName: String, segment : Boolean, loop : Boolean, loop_size : Int): String = {
+  def generate(nseqs: Int, memsize: Int, fprnd : Int, mix: Map[String,Int], veccfg: Map[String,String], use_amo: Boolean, use_mul: Boolean, use_div: Boolean, outDirName: String, outFileName: String, segment : Boolean, loop : Boolean, loop_size : Int): String = {
     assert (mix.values.sum == 100, println("The instruction mix specified in config does not add up to 100%"))
     assert (mix.keys.forall(List("xmem","xbranch","xalu","fgen","fpmem","fax","fdiv","vec") contains _), println("The instruction mix specified in config contains an unknown sequence type name"))
 
@@ -61,12 +62,12 @@ object Generator extends App
     SeqVec.cnt = 0
     val s = prog.generate(nseqs, fprnd, mix, veccfg, use_amo, use_mul, use_div, segment, loop, loop_size)
 
-    val oname = "output/" + outFileName + ".S"
+    val oname = outDirName + "/" + outFileName + ".S"
     val fw = new FileWriter(oname)
     fw.write(s)
     fw.close()
     val stats = prog.statistics(nseqs,fprnd,mix,vnseq,vmemsize,vfnum,vecmix,use_amo,use_mul,use_div)
-    val sname = "output/" + outFileName + ".stats"
+    val sname = outDirName + "/" + outFileName + ".stats"
     val fw2 = new FileWriter(sname)
     fw2.write(stats)
     fw2.close()
