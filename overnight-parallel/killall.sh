1143e69ae1f83dd8d97f13f7b9de20f06efe7d69
Zihao Yu <yuzihao@ict.ac.cn>
2019-09-13 08:03:51 +0000

    overnight-parallel: merge small tools into Makefile

--- overnight-parallel/killall.sh
+++
@@ -1,3 +0,0 @@
-pkill -9 python
-pkill -9 java
-pkill -9 qemu
--- overnight-parallel/total-pass.sh
+++
@@ -1,3 +0,0 @@
-# count the total number of tests passed and total number of instructions executed
-
-find . -name 'log.txt' | xargs cat | grep pass | awk '{t+=$2; i+=$4}END{print t, i}'
